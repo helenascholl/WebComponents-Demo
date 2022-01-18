3790c4c43543786d15ba7becfc55c755cc518aec
schollsebastian <scholl.sebastian@gmx.at>
2022-01-18 17:08:15 +0100

    Add EventListeners

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 import './hello-component.js';
 
+window.addEventListener('DOMContentLoaded', () => {
+  let button = document.getElementById('button');
+  let main = document.getElementById('main');
+  let hello = document.querySelector('hello-component');
+
+  button.addEventListener('click', () => {
+    main.innerText = 'Cool content';
+    hello.setAttribute('text', 'Hello component!');
+  });
+});
+
 console.log('loaded');
