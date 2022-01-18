84922780c610030839be101a6afb7db7205bca20
schollsebastian <scholl.sebastian@gmx.at>
2022-01-18 17:07:57 +0100

    Implement callback methods

--- src/hello-component.js
+++ src/hello-component.js
@@ -1,8 +1,25 @@
-class HelloComponent extends HTMLElement{
+class HelloComponent extends HTMLElement {
+
+  static get observedAttributes() {
+    return [ 'text' ];
+  }
 
   constructor() {
     super();
     console.log('HelloComponent constructor');
+    this.attachShadow({ mode: 'open' });
+  }
+
+  connectedCallback() {
+    console.log('connected');
+
+    const div = document.createElement('div');
+    div.innerHTML = 'Hello World!';
+    this.shadowRoot.appendChild(div);
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    console.log(`attribute ${name} changed to ${newValue}`);
   }
 
 }
