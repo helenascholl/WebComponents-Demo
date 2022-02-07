f39da5c591ca0eb23685ab1838b2226e73ae5bde
schollsebastian <scholl.sebastian@gmx.at>
2022-02-07 22:18:55 +0100

    Import TodoComponent

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './hello-component.js';
 import './parent-component.js';
 import './list-component.js';
+import './todo-component.js';
 
 window.addEventListener('DOMContentLoaded', () => {
   console.log('DOM content loaded');
